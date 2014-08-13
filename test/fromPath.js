e75b8f678814951f98d71bd3337feee87e803aff
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-08-13 13:33:08 +0600

    Add path to properties

--- test/fromPath.js
+++ test/fromPath.js
@@ -8,13 +8,12 @@ var block = path.join(__dirname, './fixtures/block/__elem/_mod');
 
 describe('BEMobject.fromPath', function () {
     it('should construct BEM object from path', function () {
-        fromPath(block)
-            .should.eql({
-                block: 'block',
-                elem: 'elem',
-                mod: 'mod',
-                level: 'fixtures',
-                bem: 'block__elem_mod'
-            });
+        var bem = fromPath(block);
+        bem.should.have.property('block', 'block');
+        bem.should.have.property('elem', 'elem');
+        bem.should.have.property('mod', 'mod');
+        bem.should.have.property('level', 'fixtures');
+        bem.should.have.property('bem', 'block__elem_mod');
+        bem.should.have.property('path');
     });
 });
