469e1961a8e6dd8914af309a71497870f2eaa6fb
Vladimir Starkov <matmuchrapna@gmail.com>
2014-08-18 11:59:22 +0000

    test(expand): move expand to object

--- test/expand.js
+++ test/expand.js
@@ -1,28 +1,27 @@
 /* global describe, it */
 
 var BEMObject = require('../lib/object.js');
-var expand = require('../lib/deps.js').expand;
 require('should');
 
-describe.only('deps.expand', function () {
+describe.only('object.expand', function () {
 
     describe('with context object', function () {
         it('should take elem from context object, if mod is defined', function () {
-            expand({ mod: 'mod' }, new BEMObject({
+            new BEMObject({
                 level: 'level',
                 block: 'block',
                 elem: 'elem'
-            })).should.eql({
+            }).expand({ mod: 'mod' }).should.eql({
                 level: 'level', block: 'block', elem: 'elem', mod: 'mod'
             });
         });
         
         it('should not take elem from context object, if block and mod is defined', function () {
-            expand({ block: 'block', mod: 'mod' }, new BEMObject({
+            new BEMObject({
                 level: 'level',
                 block: 'block',
                 elem: 'elem'
-            })).should.eql({
+            }).expand({ block: 'block', mod: 'mod' }).should.eql({
                 level: 'level', block: 'block', mod: 'mod'
             });
         });
@@ -36,70 +35,72 @@ describe.only('deps.expand', function () {
                 value: 'two'
             });
             
-            expand({ block: 'one' }, bem).should.eql({
+            bem.expand({ block: 'one' }).should.eql({
                 level: 'two', block: 'one'
             });
             
-            expand({ elem: 'one' }, bem).should.eql({
+            bem.expand({ elem: 'one' }).should.eql({
                 level: 'two', block: 'two', elem: 'one'
             });
             
-            expand({ mod: 'one' }, bem).should.eql({
+            bem.expand({ mod: 'one' }).should.eql({
                 level: 'two', block: 'two', elem: 'two', mod: 'one'
             });
             
-            expand({ value: 'one' }, bem).should.eql({
+            bem.expand({ value: 'one' }).should.eql({
                 level: 'two', block: 'two', elem: 'two', mod: 'two', value: 'one'
             });
         });
         
         it('should not override properties from context, if they are in deps object', function () {
-            expand({ level: 'one', block: 'one', elem: 'one', mod: 'one', value: 'one' }, new BEMObject({
+            new BEMObject({
                 level: 'two',
                 block: 'two',
                 elem: 'two',
                 mod: 'two',
                 value: 'two'
-            })).should.eql({
+            }).expand({ level: 'one', block: 'one', elem: 'one', mod: 'one', value: 'one' }).should.eql({
                 level: 'one', block: 'one', elem: 'one', mod: 'one', value: 'one'
             });
         });
     });
 
+    var bem = new BEMObject();
+
     it('should throw expection on empty object', function () {
         (function () {
-            expand({});    
+            bem.expand({});    
         }).should.throw();
     });
 
     it('should properly handle single elem', function () {
-        expand({ elem: 'singleElem' }).should.eql({
+        bem.expand({ elem: 'singleElem' }).should.eql({
             elem: 'singleElem'
         });
     });
     
     it('should properly handle multiply elements', function () {
-        expand({ elems: ['row', 'cell'] }).should.eql([
+        bem.expand({ elems: ['row', 'cell'] }).should.eql([
             { elem: 'row' },
             { elem: 'cell' }
         ]);
     });
     
     it('should properly handle single mod with value', function () {
-        expand({ mod: 'color', value: 'white' }).should.eql({
+        bem.expand({ mod: 'color', value: 'white' }).should.eql({
             mod: 'color', value: 'white'
         });
     });
     
     it('should properly handle multiple mods with single values', function () {
-        expand({ mods: { color: 'white', position: 'top' }}).should.eql([
+        bem.expand({ mods: { color: 'white', position: 'top' }}).should.eql([
             { mod: 'color', value: 'white' },
             { mod: 'position', value: 'top' }
         ]);
     });
     
     it('should properly handle multiple mods with multiple values', function () {
-        expand({ mods: { color: 'white', position: ['top', 'bottom'] }}).should.eql([
+        bem.expand({ mods: { color: 'white', position: ['top', 'bottom'] }}).should.eql([
             { mod: 'color', value: 'white' },
             { mod: 'position', value: 'top' },
             { mod: 'position', value: 'bottom' }
@@ -107,8 +108,8 @@ describe.only('deps.expand', function () {
     });
     
     it('should throw exception when both `elem` and `elems` defined (also `mod` and `mods`)', function () {
-        expand({ elem: '', elems: [] }).should.throw();
-        expand({ mod: '', mods: [] }).should.throw();
+        (function() { bem.expand({ elem: '', elems: [] }) }).should.throw();
+        (function() { bem.expand({ mod: '', mods: [] }) }).should.throw();
     });
     
 });
